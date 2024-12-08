da3275cbdc65f3b3f31c934b1847b74ce9ad92e2
Kris Hall <kjh@ieee.org>
2024-12-08 11:42:35 -0500

    build(p): update version number for release

--- project.clj
+++ project.clj
@@ -17,7 +17,7 @@
 ;;	   project site: https://github.com/kineticfire-labs/clojure-collections
 
 
-(defproject com.kineticfire/collections "1.1.0-SNAPSHOT"
+(defproject com.kineticfire/collections "1.1.0"
   :description "Clojure/ClojureScript utilities for collections"
   :url "https://github.com/kineticfire-labs/clojure-collections"
   :license {:name "Apache License, Version 2.0"
